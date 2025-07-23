10e94bf7d36d76234d715ba1d8561e2f2765aa0b
Greydawn Dewer <109224434+greydawndewer@users.noreply.github.com>
2025-07-24 03:27:18 +0530

    a

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.use('/companies', comps);
+app.use('/', comps);
 
 
 // Start server
